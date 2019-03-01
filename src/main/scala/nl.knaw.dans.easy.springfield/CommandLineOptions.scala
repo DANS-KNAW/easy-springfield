ba02e6635dec8b689cdbee625043ac7b7cde2d74
Richard van Heest <rvanheest@users.noreply.github.com>
2019-03-01 14:53:35 +0100

    Update src/main/scala/nl.knaw.dans.easy.springfield/CommandLineOptions.scala
    Co-Authored-By: stephanHogenboom <24325101+stephanHogenboom@users.noreply.github.com>

--- src/main/scala/nl.knaw.dans.easy.springfield/CommandLineOptions.scala
+++ src/main/scala/nl.knaw.dans.easy.springfield/CommandLineOptions.scala
@@ -280,7 +280,7 @@ class CommandLineOptions(args: Array[String], properties: PropertiesConfiguratio
       descr = "path to the WebVTT subtitles file to add",
       required = true)
     validate(languageCode)(lc => if (languageCodes.contains(lc)) Right(())
-                                 else Left(s"$lc is not an ISO639-1 supported language code")) //TODO compiler warning: a pure expression does nothing in statement position
+                                 else Left(s"$lc is not an ISO639-1 supported language code"))
     footer(SUBCOMMAND_SEPARATOR)
   }
   addSubcommand(addSubtitlesToVideo)
