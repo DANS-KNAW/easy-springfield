f1db28cef5914dba347a78ed7e7eea1ae18a7a4e
Stephan Hogenboom <stephan.hogenboom@dans.knaw.nl>
2019-03-13 15:58:00 +0100

    added a bunch of unit tests

--- src/test/scala/nl.knaw.dans.easy.springfield/Smithers2Spec.scala
+++ src/test/scala/nl.knaw.dans.easy.springfield/Smithers2Spec.scala
@@ -16,11 +16,12 @@
 package nl.knaw.dans.easy.springfield
 
 import java.net.URI
+import java.nio.file.Paths
 
 import nl.knaw.dans.lib.logging.DebugEnhancedLogging
 import org.scalatest._
 
-import scala.util.{ Failure, Success }
+import scala.util.{ Failure, Random, Success }
 import scala.xml.Elem
 
 class Smithers2Spec extends FlatSpec
@@ -75,4 +76,104 @@ class Smithers2Spec extends FlatSpec
       </fsxml>
     extractVideoRefFromPresentationForVideoId("1")(elemDuplicateReference) shouldBe Success("domain/dans/user/utest/video/5")
   }
+
+  "checkPresentation" should "succeed if the name path has more than 3 parts and presentation is the penultimate part" in {
+    checkPresentation(Paths.get("domain/dans/user/utest/presentation/1")) shouldBe Success(())
+  }
+
+  it should "fail if the path has more than 3 parts and presentation is the last part" in {
+    val path = "domain/dans/user/utest/presentation"
+    checkPresentation(Paths.get(path)) should matchPattern {
+      case Failure(iae: IllegalArgumentException) if iae.getMessage == createExceptionMessage(path) =>
+    }
+  }
+
+  it should "fail if the  path has less than 4 parts and presentation is the penultimate part" in {
+    val path = "utest/presentation/notANumber"
+    checkPresentation(Paths.get(path)) should matchPattern {
+      case Failure(iae: IllegalArgumentException) if iae.getMessage == createExceptionMessage(path) =>
+    }
+  }
+
+  it should "succeed if the path has more than 3 parts and presentation is the penultimate part, even though last part is not a number" in {
+    checkPresentation(Paths.get("domain/dans/user/utest/presentation/notANumber")) shouldBe Success(())
+  }
+
+  "checkVideoReferId" should "succeed if the path has more than 3 parts and video is the penultimate part" in {
+    checkVideoReferId(Paths.get("domain/dans/user/utest/video/1")) shouldBe Success(())
+  }
+
+  it should "fail if the path has more than 3 parts and video is the last part" in {
+    val path = "domain/dans/user/utest/video"
+    checkVideoReferId(Paths.get(path)) should matchPattern {
+      case Failure(iae: IllegalArgumentException) if iae.getMessage == s"$path does not appear to be a video referid. Expected format: [domain/<d>/]user/<u>/video/<number>" =>
+    }
+  }
+
+  it should "fail if the path has less than 4 parts and video is the penultimate part" in {
+    val path = "test/video/1"
+    checkVideoReferId(Paths.get(path)) should matchPattern {
+      case Failure(iae: IllegalArgumentException) if iae.getMessage == s"$path does not appear to be a video referid. Expected format: [domain/<d>/]user/<u>/video/<number>" =>
+    }
+  }
+
+  it should "succeed if the path has more than 3 parts and video is the penultimate part, even though last part is not a number" in {
+    checkVideoReferId(Paths.get("domain/dans/user/utest/video/notANumber")) shouldBe Success(())
+  }
+
+  "checkCollection" should "succeed if the name path has more than 3 parts and collection is the penultimate part" in {
+    checkCollection(Paths.get("domain/dans/user/utest/collection/1")) shouldBe Success(())
+  }
+
+  it should "fail if the path has more than 3 parts and collection is the last part" in {
+    val path = "domain/dans/user/utest/collection"
+    checkCollection(Paths.get(path)) should matchPattern {
+      case Failure(iae: IllegalArgumentException) if iae.getMessage == s"$path does not appear to be a collection Springfield path. Expected format: [domain/<d>/]user/<u>/collection/<name>" =>
+    }
+  }
+
+  it should "fail if the path has less than 4 parts and collection is the penultimate part" in {
+    val path = "test/collection/1"
+    checkCollection(Paths.get(path)) should matchPattern {
+      case Failure(iae: IllegalArgumentException) if iae.getMessage == s"$path does not appear to be a collection Springfield path. Expected format: [domain/<d>/]user/<u>/collection/<name>" =>
+    }
+  }
+
+  it should "succeed if the path has more than 3 parts and collection is the penultimate part, even though last part is not a number" in {
+    checkCollection(Paths.get("domain/dans/user/utest/collection/notANumber")) shouldBe Success(())
+  }
+
+  "checkNameLenght" should "succeed if the name length is below 101" in {
+    checkNameLength("below 100") shouldBe Success(())
+    checkNameLength(Random.nextString(100)) shouldBe Success(())
+  }
+
+  it should "fail if the name length is above 100" in {
+    val moreThan100Chars = Random.nextString(101)
+    checkNameLength(moreThan100Chars) should matchPattern {
+      case Failure(i: IllegalArgumentException) if i.getMessage == s"Name is longer than 100 chars: $moreThan100Chars" =>
+    }
+  }
+
+  "getCompletePath" should "add domain to the path" in {
+    val uncompletPath = Paths.get("not/a/complete/path")
+    getCompletePath(uncompletPath) shouldBe Paths.get("domain/dans").resolve(uncompletPath)
+  }
+
+  it should "not alter an already complete path (relative)" in {
+    val completePath = Paths.get("domain/dans/complete/path")
+    getCompletePath(completePath) shouldBe completePath
+  }
+
+  it should "not alter an already complete path (absolute)" in {
+    val completePath = Paths.get("/domain/dans/complete/path")
+    getCompletePath(completePath) shouldBe completePath
+  }
+
+  it should "not alter an already complete path (relative), also if the second param is not equal to the default domain" in {
+    val completePath = Paths.get("domain/notDans/complete/path")
+    getCompletePath(completePath) shouldBe completePath
+  }
+
+  private def createExceptionMessage(path: String): String = s"$path does not appear to be a presentation referid or Springfield path. Expected format: [domain/<d>/]user/<u>/presentation/<number> OR [domain/<d>/]user/<u>/collection/<c>/presentation/<p>"
 }
